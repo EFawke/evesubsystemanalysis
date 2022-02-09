11cd651172107c8925bf5981fd906f47bc2a777d
Eduardo Fawke <teduardof@gmail.com>
2022-02-09 18:50:41 +0000

    first commit

--- backend/app.js
+++ backend/app.js
@@ -16,3 +16,13 @@ app.use(cors());
 app.use(errorHandler());
 
 app.listen(port, () => console.log(`listening on port ${port}`));
+
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/src')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/public/index.html'))
+})
