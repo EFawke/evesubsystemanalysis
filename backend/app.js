663045392ef9aa8d84c2d70f8ea335e08abeecbe
Eduardo Fawke <teduardof@gmail.com>
2022-02-11 06:35:31 +0000

    npm run dev

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ const path = require('path')
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '/../frontend/src/App.js')))
 
-AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../frontend/public/index.html'))
 })
