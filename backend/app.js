9eee23e5535f8ef474acdb7e761005f66fc3519d
Eduardo Fawke <teduardof@gmail.com>
2022-02-11 04:50:43 +0000

    point to app.js

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.listen(port, () => console.log(`listening on port ${port}`));
 const path = require('path')
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, '../frontend/src/App.js')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
