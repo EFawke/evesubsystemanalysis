95383f3efbd3012468c7f0741801d2f989a5e71e
Eduardo Fawke <teduardof@gmail.com>
2022-02-12 05:30:04 +0000

    gah

--- backend/utils/zkillTableInit.js
+++ backend/utils/zkillTableInit.js
@@ -1,30 +1,14 @@
 // const sqlite3 = require('sqlite3');
 
 // const db = new sqlite3.Database('zkill.db');
-const { Client } = require('pg');
-
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
 const pg = require('pg')
 const pool = new pg.Pool();
 
-client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//     if (err) throw err;
-//     for (let row of res.rows) {
-//         console.log(JSON.stringify(row));
-//     }
-//     client.end();
-// });
-
 const zkillDbInit = () => {
-    pool.connect((err, client, done) => {
+    pool.connect(function (err, client, done) {
+        if(err){
+            console.log(err)
+        }
         console.log(client)
         console.log('database connected')
         client.query(`CREATE TABLE IF NOT EXISTS zkill(
