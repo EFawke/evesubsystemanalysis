d0f2d7c02dc03b73bf7694458f9c0d8d717de5e1
Eduardo Fawke <teduardof@gmail.com>
2022-02-12 05:55:36 +0000

    going back

--- backend/utils/zkillTableInit.js
+++ backend/utils/zkillTableInit.js
@@ -1,26 +1,53 @@
-// const sqlite3 = require('sqlite3');
-
-// const db = new sqlite3.Database('zkill.db');
 const pg = require('pg')
 const pool = new pg.Pool();
 
-const zkillDbInit = () => {
-    pool.connect(function (err, client, done) {
-        if(err){
-            console.log(err)
-        }
-        console.log(client)
-        console.log('database connected')
-        client.query(`CREATE TABLE IF NOT EXISTS zkill(
-        zkill_id PRIMARY KEY NOT NULL,
-        hash TEXT NOT NULL
-    );`, (err) => {
-        if(err){
-            console.log(err);
-        }
-    })
-    done();
-    })
-}
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query(`CREATE TABLE IF NOT EXISTS zkill(
+    zkill_id PRIMARY KEY NOT NULL,
+    hash TEXT NOT NULL
+);`, (err, res) => {
+  if (err) console.log(err);
+  client.end();
+});
+
+// const zkillDbInit = () => {
+//     pool.connect(function (err, client, done) {
+//         if(err){
+//             console.log(err)
+//         }
+//         console.log(client)
+//         console.log('database connected')
+//         client.query(`CREATE TABLE IF NOT EXISTS zkill(
+//             zkill_id PRIMARY KEY NOT NULL,
+//             hash TEXT NOT NULL
+//         );`, (err) => {
+//             if(err){
+//                 console.log(err);
+//             }
+//     })
+//     done();
+//     })
+// }
 
 module.exports = zkillDbInit;
+
+
+
+/*  query(`CREATE TABLE IF NOT EXISTS zkill(
+    zkill_id PRIMARY KEY NOT NULL,
+    hash TEXT NOT NULL
+);`, (err) => {
+    if(err){
+        console.log(err);
+    }
+*/
