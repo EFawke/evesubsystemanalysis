684dc205bd98e3e6722e0ae4bc70404317ff3caf
Eduardo Fawke <teduardof@gmail.com>
2022-02-14 09:16:02 +0000

    s

--- backend/app.js
+++ backend/app.js
@@ -28,12 +28,12 @@ const path = require('path')
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('frontend/build'));
+  app.use(express.static('../frontend/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname,'../', 'frontend', 'build', 'index.html'));
   });
 }
 
