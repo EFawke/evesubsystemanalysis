c0cdecbc2a52e50ee56a37b114585993d23b57c1
Eduardo Fawke <teduardof@gmail.com>
2022-02-14 08:40:07 +0000

    frontend

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ if (port == null || port == "") {
 const path = require('path')
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '/../frontend/src/App.js')))
+app.use(express.static(path.join(__dirname, '/../frontend/src/index.js')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
