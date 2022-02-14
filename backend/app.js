c8ace1851f640c8b4b6bedaf86ef0e3ea498d2e9
Eduardo Fawke <teduardof@gmail.com>
2022-02-14 08:55:43 +0000

    halp

--- backend/app.js
+++ backend/app.js
@@ -31,8 +31,7 @@ app.use(express.static(path.join(__dirname, '/../frontend/src/index.js')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname + '/../frontend/public/index.html'))
-  res.sendFile(path.join(__dirname + '/../frontend/src/index.js'))
+  res.sendFile(path.join(__dirname + '/../frontend/public/index.html'))
 
 })
 
