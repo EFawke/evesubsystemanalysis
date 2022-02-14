f063a5eb05cd88decc951859b40a87e3d799c3a2
Eduardo Fawke <teduardof@gmail.com>
2022-02-14 12:23:29 +0000

    pls load

--- frontend/src/info.js
+++ frontend/src/info.js
@@ -6,56 +6,38 @@ class Info extends React.Component {
         super(props);
         this.state = {
             isLoaded: false,
-            totalDestroyed: null,
+            // totalDestroyed: null,
             shipSelected: this.props.shipSelected,
             totalDays: null
         };
-        this.fetchTotalDestroyed = this.fetchTotalDestroyed.bind(this);
     }
     HomeCheck = () => {
-        //Homepage automatically displays Herons.
         if (window.location.pathname === "/") {
             this.setState({ shipSelected: "Heron" })
-        }
-        if (window.location.pathname === "/AllC5RattingShips") {
-            this.setState({ shipSelected: "C5 Ratting Ship" })
-        }
-        if(window.location.pathname === "/Marauders"){
-            this.setState({shipSelected : "Marauder"})
-        }
-        if(window.location.pathname === "/Dreadnoughts"){
-            this.setState({shipSelected : "Dreadnought"})
-        }
-        else {
+        } else {
             return window.location.pathname
         }
     }
-    fetchTotalDestroyed = () => {
-        //Homepage automatically displays Herons.
-        if (window.location.pathname === "/") {
-            axios.get(`/api/info/totalDestroyed/Heron`)
-            .then(response => {
-                this.setState({ totalDestroyed: response.data.totalDestroyed });
-            })
-            .then(() => {
-                this.setState({isLoaded: true})
-            })
-        }
-        else if (this.props.shipSelected === 'Marauders' || this.props.shipSelected === 'Dreadnoughts' || this.props.shipSelected === 'AllC5RattingShips') {
-            axios.get(`api/info/totalClassDestroyed/${this.props.shipSelected}`).then(response => {
-                this.setState({ totalDestroyed: response.data.totalClassDestroyed });
-            })
-        } else {
-        axios.get(`/api/info/totalDestroyed/${this.props.shipSelected}`)
-            .then(response => {
-                console.log(response)
-                this.setState({ totalDestroyed: response.data.totalDestroyed })
-            })
-            .then(() => {
-                this.setState({isLoaded: true})
-            })
-        }
-    }
+    // fetchTotalDestroyed = () => {
+    //     if (window.location.pathname === "/") {
+    //         axios.get(`/api/info/totalDestroyed/Heron`)
+    //         .then(response => {
+    //             this.setState({ totalDestroyed: response.data.totalDestroyed });
+    //         })
+    //         .then(() => {
+    //             this.setState({isLoaded: true})
+    //         })
+    //     } else {
+    //     axios.get(`/api/info/totalDestroyed/${this.props.shipSelected}`)
+    //         .then(response => {
+    //             console.log(response)
+    //             this.setState({ totalDestroyed: response.data.totalDestroyed })
+    //         })
+    //         .then(() => {
+    //             this.setState({isLoaded: true})
+    //         })
+    //     }
+    // }
     calcTotalDays = () => {
         const oneDay = 24 * 60 * 60 * 1000;
         const firstDate = new Date('2021-07-03T11:30:02Z')
@@ -64,19 +46,34 @@ class Info extends React.Component {
         this.setState({ totalDays: diffDays });
     }
     componentDidMount() {
-        this.HomeCheck();
-        this.fetchTotalDestroyed();
+        // this.HomeCheck();
+        if (window.location.pathname === "/") {
+            axios.get(`/api/info/totalDestroyed/Heron`).then(response => {
+                const totalDes = response.data;
+                this.setState({ totalDestroyed: totalDes })
+            }).then(() => {
+                this.setState({ isLoaded: true })
+            })
+        } else {
+            axios.get(`/api/info/totalDestroyed/${this.props.shipSelected}`).then(response => {
+                const totalDes = response.data;
+                this.setState({ totalDestroyed: totalDes })
+            }).then(() => {
+                this.setState({ isLoaded: true })
+            })
+        }
+        // this.fetchTotalDestroyed();
         this.calcTotalDays();
     };
     render() {
         const isLoaded = this.state.isLoaded
-        if(!isLoaded){
+        if (!isLoaded) {
             return (
                 <div>Loading...</div>
             )
         }
         return (
-            <p className="info">Fetching {this.state.totalDestroyed} <span className = "infoSpan">{this.state.shipSelected}</span> kills from the last <span>3 months</span> in wormhole space.</p>
+            <p className="info">Fetching {this.state.totalDestroyed} <span className="infoSpan">{this.state.shipSelected}</span> kills from the last <span>3 months</span> in wormhole space.</p>
         )
     }
 };
