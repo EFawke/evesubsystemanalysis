42f777449d8e3f58c73501223ec976523dedb49b
Eduardo Fawke <teduardof@gmail.com>
2022-02-16 15:15:35 +0000

    routers

--- backend/api/apiRouter.js
+++ backend/api/apiRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 const apiRouter = express.Router();
 const zkillRouter = require('./zkill.js');
 const shipTypeRouter = require('./shipTypeRouter.js')
-// const infoRouter = require('./info.js');
+const infoRouter = require('./info.js');
 // const hoverRouter = require('./hover.js');
 
 apiRouter.use('/ShipType', shipTypeRouter);
 apiRouter.use('./zkill.js', zkillRouter);
-// apiRouter.use('/counter', infoRouter);
+apiRouter.use('/counter', infoRouter);
 // apiRouter.use('/hoverRouter', hoverRouter);
 
 module.exports = apiRouter;
