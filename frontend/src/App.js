b8f80773ff860e671b1b14c3ebac4623c876628f
Eduardo Fawke <teduardof@gmail.com>
2022-02-16 15:22:38 +0000

    stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Header from './header'
 import Heatmap from './heat-map'
 import Info from './info'
-// import HoverInfo from './hover';
 
 class App extends React.Component {
   render() {
@@ -11,8 +10,7 @@ class App extends React.Component {
       <div className="background">
         <Header />
         <Heatmap shipSelected = {window.location.pathname.replace(/\//g,'')}/>
-        {/* <Info className ="info" shipSelected = {window.location.pathname.replace(/\//g,'')}/> */}
-        {/* <HoverInfo hour="05" day="Sunday" ship="Gila"/> */}
+        <Info className ="info" shipSelected = {window.location.pathname.replace(/\//g,'')}/>
       </div>
     )
   }
