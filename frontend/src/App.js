0e3447c85ee989bf7740cf86b17481fc98b36a73
Eduardo Fawke <teduardof@gmail.com>
2022-02-16 17:19:16 +0000

    removed info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import Header from './header'
 import Heatmap from './heat-map'
-import Info from './info'
+// import Info from './info'
 
 class App extends React.Component {
   render() {
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div className="background">
         <Header />
         <Heatmap shipSelected = {window.location.pathname.replace(/\//g,'')}/>
-        <Info className ="info" shipSelected = {window.location.pathname.replace(/\//g,'')}/>
+        {/* <Info className ="info" shipSelected = {window.location.pathname.replace(/\//g,'')}/> */}
       </div>
     )
   }
