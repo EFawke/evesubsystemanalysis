90f38b12a4279bc6e5fb7fdf68caa9bd49ba97ba
Eduardo Fawke <teduardof@gmail.com>
2022-02-18 19:30:19 +0000

    s

--- backend/api/zkill.js
+++ backend/api/zkill.js
@@ -71,7 +71,7 @@ const lookUpEsi = async (wormholeData, id) => {
     for (let i = 0; i < Object.keys(wormholeData).length; i++) {
         const newzKillId = Object.keys(wormholeData)[i]
         const currentHash = Object.values(wormholeData)[i]
-        if(Number(id) < Number(newzKillId)){
+        if(Number(id) > Number(newzKillId)){
             continue
         }
         await axios.get(`https://esi.evetech.net/latest/killmails/${newzKillId}/${currentHash}/?datasource=tranquility`)
