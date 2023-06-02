64ab2544af0eb937f14c5aa471bd9487d2650685
Eduardo Fawke <teduardof@gmail.com>
2023-06-02 14:47:28 +0100

    s

--- backend/api/key.js
+++ backend/api/key.js
@@ -5,7 +5,7 @@ const keyRouter = express.Router();
 
 keyRouter.get('/', (req, res, next) => {
     // Hash the API key or not, fuck it
-    const apiKey = "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";
+    const apiKey = "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";
     res.status(200).send(apiKey);
 });
 
